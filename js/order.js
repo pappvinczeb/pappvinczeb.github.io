e96ea3ea6aeab60d2869f7e7c374f8102ee0f571
pappvinczeb <Papp-Vincze.Brigitta@innomatrix.hu>
2022-02-25 17:08:25 +0100

    Sp-7 st-3

--- js/order.js
+++ js/order.js
@@ -66,9 +66,10 @@ function validateFormUserComment() {
 }
 
 
+
 //Lefuttatja a validációkat és kiírja a felületre a végső árat
 function showSumPrice() {
-    let price = 1200;
+    const price = 1200;
     let showAmount = document.querySelector("span.showAmount");
     let quantityNum = parseInt(document.querySelector("input.quantity").value);
 
@@ -79,85 +80,4 @@ function showSumPrice() {
     calcAmount(price, quantityNum)
     showAmount.innerHTML = amount;
 
-}
-
-
-//Időjárás panel
-
-let dayWeather;
-let weatherSale;
-let weatherMatrix = [["monday", 7], ["tuesday", 12], ["wednesday", 14], ["thursday", 67], ["friday", 38], ["saturday", 45], ["sunday", 52]];
-let saleMatrix = [[0, "Ma forró csokit is árusítunk!"],[15, "Melegedj át egy teával nálunk!"],[20, "Ma finom sütivel várunk!"],[25, "Ma fagyit is kínálunk!"],[50, "Hűsítsd le magad egy jéghideg limonádéval!"]];
-let smallest=parseInt(weatherMatrix[0].slice(1,2));
-let biggest=parseInt(weatherMatrix[0].slice(1,2));
-let average=0;
-
-
-function getDayWeather(){
-    let selectedDay = document.querySelector("select[name='days']");
-    let showWeather = document.querySelector("span.weather");
-
-    weatherMatrix.forEach(element => {
-        dayWeather = element[0] == selectedDay.value ? element[1] : dayWeather;
-    showWeather.innerHTML = dayWeather; 
-    });
-
-
-}
-
-function getWeatherSale (){
-    let showSale = document.querySelector("span.weatherSale");
-
-    saleMatrix.forEach(element => {
-        weatherSale= element[0]<=dayWeather ? element[1] : weatherSale;
-    showSale.innerHTML = weatherSale; 
-    })
-}
-
-
-
-function getMinTemperature(){
-    let showMinTemp = document.querySelector("span.temperatureMin")
-    for(let i=0; i < weatherMatrix.length; i++){
-        if(parseInt(weatherMatrix[i].slice(1,2)) < smallest){
-            smallest = parseInt(weatherMatrix[i].slice(1,2))
-        }
-    }
-    showMinTemp.innerHTML = smallest
-}
-
-function getMaxTemperature(){
-    let showMinTemp = document.querySelector("span.temperatureMax")
-    for(let i=0; i < weatherMatrix.length; i++){
-        if(parseInt(weatherMatrix[i].slice(1,2)) > biggest){
-            biggest = parseInt(weatherMatrix[i].slice(1,2))
-        }
-    }
-    showMinTemp.innerHTML = biggest
-}
-
-function getAverageTemperature(){
-    let showAverageTemp = document.querySelector("span.temperatureAverage")
-    let sum=0;
-    let piece=1;
-
-    for(let i=0; i < weatherMatrix.length; i++){
-        sum += parseInt(weatherMatrix[i].slice(1,2));
-    }
-
-    for(let i=0; i < weatherMatrix.length; i++){
-        piece ++;
-    }
-    average=sum/(piece-1)
-    showAverageTemp.innerHTML = parseInt(average)
-
-}
-
-
-function getWeatherAndSale(){
-    getDayWeather()
-    getWeatherSale ()
-    getMinTemperature ()
-    getMaxTemperature()
-    getAverageTemperature()
 }
