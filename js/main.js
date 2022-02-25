731df31868006eba1888400ff6dc362e0557e1fe
pappvinczeb <Papp-Vincze.Brigitta@innomatrix.hu>
2022-02-25 15:20:50 +0100

    Sp-7 st-2

--- js/main.js
+++ js/main.js
@@ -86,8 +86,12 @@ function showSumPrice() {
 
 let dayWeather;
 let weatherSale;
-let weatherMatrix = [["monday", 0], ["tuesday", 15], ["wednesday", 14], ["thursday", 20], ["friday", 38], ["saturday", 45], ["sunday", 52]];
+let weatherMatrix = [["monday", 7], ["tuesday", 12], ["wednesday", 14], ["thursday", 67], ["friday", 38], ["saturday", 45], ["sunday", 52]];
 let saleMatrix = [[0, "Ma forró csokit is árusítunk!"],[15, "Melegedj át egy teával nálunk!"],[20, "Ma finom sütivel várunk!"],[25, "Ma fagyit is kínálunk!"],[50, "Hűsítsd le magad egy jéghideg limonádéval!"]];
+let smallest=parseInt(weatherMatrix[0].slice(1,2));
+let biggest=parseInt(weatherMatrix[0].slice(1,2));
+let average=0;
+
 
 function getDayWeather(){
     let selectedDay = document.querySelector("select[name='days']");
@@ -110,7 +114,45 @@ function getWeatherSale (){
     })
 }
 
+
+
+function getMinTemperature(){
+    let showMinTemp = document.querySelector("span.temperatureMin")
+    for(let i=0; i < weatherMatrix.length; i++){
+        if(parseInt(weatherMatrix[i].slice(1,2)) < smallest){
+            smallest = parseInt(weatherMatrix[i].slice(1,2))
+        }
+    }
+    showMinTemp.innerHTML = smallest
+}
+
+function getMaxTemperature(){
+    let showMinTemp = document.querySelector("span.temperatureMax")
+    for(let i=0; i < weatherMatrix.length; i++){
+        if(parseInt(weatherMatrix[i].slice(1,2)) > biggest){
+            biggest = parseInt(weatherMatrix[i].slice(1,2))
+        }
+    }
+    showMinTemp.innerHTML = biggest
+}
+
+function getAverageTemperature(){
+    let showAverageTemp = document.querySelector("span.temperatureAverage")
+    let sum=0;
+    let piece=1;
+
+    for(let i=0; i < weatherMatrix.length; i++){
+        sum += parseInt(weatherMatrix[i].slice(1,2));
+    }
+    average=sum/(piece-1)
+    showAverageTemp.innerHTML = average
+}
+
+
 function getWeatherAndSale(){
     getDayWeather()
     getWeatherSale ()
+    getMinTemperature ()
+    getMaxTemperature()
+    getAverageTemperature()
 }
